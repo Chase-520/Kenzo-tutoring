977884ff8e8bd365973d6fa6cc085777489704cf
Chase-520 <chace070528@gmail.com>
2024-09-20 16:46:03 -0700

    practice update

---
+++ practice/logical operators.py
@@ -0,0 +1,46 @@
+"""
+In python, we have operator when using if statements.
+"""
+
+a = True
+b = False
+c = 5
+d = 7
+
+"""
+comparing numbers
+"""
+result1 = 5<7
+print(result1)
+
+result2 = 6==7
+print(result2)
+
+result3 = 11>=7
+print(result3)
+
+
+
+var1 = (   and   )
+print(var1)
+
+var2 = (   or   )
+print(var2)
+
+var3 = (   and   or   )
+print(var3)
+
+
+
+"""
+if(boolean):
+    **content**
+elif(boolean):
+    **content**
+else:
+    **content**
+"""
+if (True):
+    print("True")
+else:
+    print("False")
