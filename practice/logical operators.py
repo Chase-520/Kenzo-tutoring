08569c6d59a02c2bc16ec04715b8f468f980c349
Chase-520 <chace070528@gmail.com>
2024-09-20 17:37:23 -0700

    0920

--- practice/logical operators.py
+++ practice/logical operators.py
@@ -2,18 +2,18 @@
 In python, we have operator when using if statements.
 """
 
-a = True
-b = False
-c = 5
-d = 7
+a = True  # boolean
+b = False # boolean
+c = 5     # int
+d = 7     # int
 
 """
 comparing numbers
 """
-result1 = 5<7
+result1 = 5<7    # 5<7 this expression returns a boolean value -> True/ False
 print(result1)
 
-result2 = 6==7
+result2 = "Car"=="Car"    # we use == to check whether two variables are the same
 print(result2)
 
 result3 = 11>=7
@@ -21,13 +21,27 @@ print(result3)
 
 
 
-var1 = (   and   )
+
+var1 = ((5<7) and (50>7))
 print(var1)
 
-var2 = (   or   )
+var2 = ((5<7) or (7<10))
 print(var2)
 
-var3 = (   and   or   )
+var3 = ((5<10) and (10>=8))or (True)
+"""
+() or ()
+|
+((5<10)and(10>=8))  -> True
+|
+(True) or (True)    -> True
+
+1> (something) or (something)
+2> (5<10) -> True
+3> (10>=8) -> True
+4> (5<10) and (10>=8) -> True
+5> True or True   -> True
+"""
 print(var3)
 
 
@@ -40,7 +54,8 @@ elif(boolean):
 else:
     **content**
 """
-if (True):
+if ("Car"=="car"):
     print("True")
 else:
     print("False")
+
