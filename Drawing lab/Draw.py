19edfa17f50f1f7dcc15e4f730469e6663132ccd
Chase-520 <chace070528@gmail.com>
2024-12-22 10:56:05 -0800

    drawing lab 1

--- Drawing lab/Draw.py
+++ Drawing lab/Draw.py
@@ -4,9 +4,22 @@ import turtle
 screen = turtle.Screen()  # create the canvas(the worl)
 t = turtle.Turtle()   # Create turtle object
 
-t.forward(100)  # Move forward 100 units
-t.right(90)     # Turn right by 90 degrees
-t.forward(50)   # Move forward 50 units
+
+for i in range(4):
+    t.goto(0, 0)
+    t.forward(100)
+    t.right(90)
+
+def drawSquare(x,y,size):
+    t.penup()
+    t.goto(x,y)
+    t.pendown()
+    for i in range(4):
+        t.forward(size)
+        t.right(90)
+
+drawSquare(x=-200,y=200,size=206)
+
 
 # This is the end of the drawing lab
 turtle.done()
