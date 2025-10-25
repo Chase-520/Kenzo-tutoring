e3b59ed530fd9aaa150feda36c5c74a2f20d8c56
PKMNjokes <138265924+PKMNjokes@users.noreply.github.com>
2025-10-24 17:12:53 -0700

    Some stuff

--- simulation/ball_drop/Ball.py
+++ simulation/ball_drop/Ball.py
@@ -5,17 +5,22 @@ class Ball:
     def __init__(self, x, y, radius=10, color=(random.randint(0,255), 0, 0)):
         self.x = x
         self.y = y
-        self.vx = 0#random.randint(-5, 5)
-        self.vy = 0#random.randint(-5, 5)
+        self.vx = random.randint(-50,10)#random.randint(-5, 5)
+        self.vy = random.randint(-50,10)#random.randint(-5, 5)
         self.gravity = 0.5
         self.radius = radius
         self.color = color
 
     def update(self, width, height):
         # apply gravity
-
+        self.vy = self.vy + self.gravity
         # Bouncing logic
-
+        if self.x >= 800:
+            self.vx = -self.vx
+        if self.y >= 600:
+            self.vy = -self.vy
+        self.x = self.x + self.vx
+        self.y = self.y + self.vy
 
         pass
 
