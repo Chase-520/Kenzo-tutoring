df572cf727fbad1ef071dc1498e267366e1ec71f
Chase <chace070528@gmail.com>
2025-11-21 17:05:18 -0800

    update duck hunt game

--- duckhunt/src/main.py
+++ duckhunt/src/main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
-from player import Player
+from duck import Duck
+from gifLoader import GIF
 
 WIDTH, HEIGHT = 800, 600
 WHITE = (255, 255, 255)
@@ -8,14 +9,15 @@ WHITE = (255, 255, 255)
 def collided(pointer, obj):
     """Return True if two objects collide based on their bounding boxes."""
     x,y = pointer
-    frame, pos = obj.update(input=None)
+    frame, pos = obj.update()
     rect = frame.get_rect(center=(pos[0], pos[1]))
     return rect.collidepoint(x, y)
 
-def update_draw(screen, obj, input=None):
-    frame, pos = obj.update(input=None)
+def update_draw(screen, obj):
+    frame, pos = obj.update()
     rect = frame.get_rect(center=(pos[0], pos[1]))
     screen.blit(frame,rect)
+
 
 class Game:
     def __init__(self):
@@ -23,27 +25,34 @@ class Game:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Duck Hunt with Dog")
         self.clock = pygame.time.Clock()
-        self.duck = Player()
+        self.duck = Duck()
         self.running = True 
+        self.bg = GIF(r"C:\Users\Chaser\Documents\git\Kenzo-tutoring\duckhunt\img\pixelBG.png")
+
 
     def run(self):
         while self.running:
+            
             dt = self.clock.tick(30)
             self.screen.fill(WHITE)
+
+
+            rect = self.bg.get_frame().get_rect(center=self.screen.get_rect().center)
+            self.screen.blit(self.bg.get_frame(),rect)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     x, y = pygame.mouse.get_pos()
                     if collided([x,y],self.duck):
-                        self.duck.change_state()
+                        self.duck.update("hit")
                     
-            
-
             
             update_draw(self.screen, self.duck)
             pygame.display.flip()
             
+
 
 if __name__ == "__main__":
     game = Game()
