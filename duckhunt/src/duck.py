3a4b3571266d3fc889d40d07b2809bb9dbf43e70
Chase <chace070528@gmail.com>
2025-11-21 17:16:27 -0800

    update to relative paths

--- duckhunt/src/duck.py
+++ duckhunt/src/duck.py
@@ -6,7 +6,7 @@ class Duck:
         self.y = random.randint(100,500)
         self.vx = random.randint(0,15)
         self.vy = random.randint(0,15)
-        self.img = GIF(r"C:\Users\Chaser\Documents\git\Kenzo-tutoring\duckhunt\img\Chibi.gif",scale=0.3)
+        self.img = GIF(r"duckhunt\img\Chibi.gif",scale=0.3)
 
         self.actions = ["hit"]
         self.states = ["escape", "die", "disappear"]
@@ -47,7 +47,7 @@ class Duck:
             # switch to die animation and start timer
             if self.y <=600: # before it hit the ground (y=600)
                 if self.timer ==0:
-                    self.img = GIF(r"C:\Users\Chaser\Documents\git\Kenzo-tutoring\duckhunt\img\Clap.gif", scale=0.3)
+                    self.img = GIF(r"duckhunt\img\Clap.gif", scale=0.3)
                     self.vy = -40
                 self.vy += 3 # add gravity
                 self.y +=  self.vy
@@ -57,7 +57,7 @@ class Duck:
                 self.vy = 0
                 self.timer = 0
 
-                self.img = GIF(r"C:\Users\Chaser\Documents\git\Kenzo-tutoring\duckhunt\img\Chibi.gif",scale=0.3)
+                self.img = GIF(r"duckhunt\img\Chibi.gif",scale=0.3)
 
                 self.current_state = "disappear"
                 self.prev_state = "die"
